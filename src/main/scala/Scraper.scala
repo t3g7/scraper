82c3b352028243f14c70dc5a78ba220019fbfc33
eftov <b.fovet@gmail.com>
2016-01-21 17:37:50 +0100

    Scrape content every 60s with loop and sleep

--- src/main/scala/Scraper.scala
+++ src/main/scala/Scraper.scala
@@ -10,6 +10,7 @@ import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import org.jsoup.nodes.Element
 
 object Scraper {
+
   def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf()
@@ -17,78 +18,81 @@ object Scraper {
       .setAppName("OFScraper")
       .set("spark.cassandra.connection.host", "localhost")
     val sc = new SparkContext(conf)
-    val ssc = new StreamingContext(sc, Seconds(5))
 
+    while (true) {
+      val browser = new Browser
+      val baseUrl = "http://communaute.orange.fr"
 
-    val browser = new Browser
-    val baseUrl = "http://communaute.orange.fr"
+      val subForums = Seq(
+        "/forums/unansweredtopicspage", // Sujets sans réponse
 
-    val subForums = Seq(
-      "/forums/unansweredtopicspage", // Sujets sans réponse
+        // internet & fixe :
+        "/les-offres-Internet-Orange-et/bd-p/ADSL", // les offres Internet Orange et options
+        "/gérer-mon-offre-Internet/bd-p/offre", // gérer mon offre Internet
+        "/homelive/bd-p/domotique", // homelive
+        "/mon-mail-Orange/bd-p/mail", // mon mail Orange
+        "/ma-connexion/bd-p/connexion", // ma connexion
+        "/mon-téléphone-par-internet-et/bd-p/tel", // mon téléphone par internet et fixe
+        "/protéger-mes-données-et-mon/bd-p/securite", // protéger mes données et mon accès internet
+        "/mes-services-Orange/bd-p/services", // mes services Orange
 
-      // internet & fixe :
-      "/les-offres-Internet-Orange-et/bd-p/ADSL", // les offres Internet Orange et options
-      "/gérer-mon-offre-Internet/bd-p/offre", // gérer mon offre Internet
-      "/homelive/bd-p/domotique", // homelive
-      "/mon-mail-Orange/bd-p/mail", // mon mail Orange
-      "/ma-connexion/bd-p/connexion", // ma connexion
-      "/mon-téléphone-par-internet-et/bd-p/tel", // mon téléphone par internet et fixe
-      "/protéger-mes-données-et-mon/bd-p/securite", // protéger mes données et mon accès internet
-      "/mes-services-Orange/bd-p/services", // mes services Orange
+        // TV d'Orange
+        "/TV-par-ADSL-et-Fibre/bd-p/maTV", // TV par ADSL et Fibre
+        "/TV-par-Satellite/bd-p/TV", // TV par Satellite
+        "/gerer-mon-offre-TV/bd-p/gerer", // gerer mon offre TV
+        "/regarder-la-TV-sur-mon-PC-ma/bd-p/webtv", // regarder la TV sur mon PC
+        "/TV-à-la-demande-OCS-et-VOD/bd-p/OCS", // TV à la demande, OCS et VOD
 
-      // TV d'Orange
-      "/TV-par-ADSL-et-Fibre/bd-p/maTV", // TV par ADSL et Fibre
-      "/TV-par-Satellite/bd-p/TV", // TV par Satellite
-      "/gerer-mon-offre-TV/bd-p/gerer", // gerer mon offre TV
-      "/regarder-la-TV-sur-mon-PC-ma/bd-p/webtv", // regarder la TV sur mon PC
-      "/TV-à-la-demande-OCS-et-VOD/bd-p/OCS", // TV à la demande, OCS et VOD
+        // mobile Orange
+        "/offres-mobile-Orange-et-options/bd-p/offres", // offres mobile Orange et options
+        "/l-iPhone-et-ses-applications/bd-p/iphone-et-ses-applications", // l'iPhone et ses applications
+        "/clé-3G-Domino-et-Tablettes/bd-p/tablettes", // clé 3G Domino et Tablettes
+        "/utiliser-mon-mobile/bd-p/iphone", // utiliser mon mobile
+        "/gérer-mon-offre-mobile/bd-p/compte" // gérer mon offre mobile
+      )
 
-      // mobile Orange
-      "/offres-mobile-Orange-et-options/bd-p/offres", // offres mobile Orange et options
-      "/l-iPhone-et-ses-applications/bd-p/iphone-et-ses-applications", // l'iPhone et ses applications
-      "/clé-3G-Domino-et-Tablettes/bd-p/tablettes", // clé 3G Domino et Tablettes
-      "/utiliser-mon-mobile/bd-p/iphone", // utiliser mon mobile
-      "/gérer-mon-offre-mobile/bd-p/compte" // gérer mon offre mobile
-    )
+      val subForumPage = browser.get("http://communaute.orange.fr/t5/ma-connexion/bd-p/connexion")
 
-    val subForumPage = browser.get("http://communaute.orange.fr/t5/ma-connexion/bd-p/connexion")
+      // Extract the thread rows
+      val threadTitlesItems: List[Element] = subForumPage >> elementList(".lia-list-row-thread-unread")
+      val threadTitles: List[String] = threadTitlesItems.map(_ >> text("h3"))
+      val threadRelLinks: List[String] = threadTitlesItems.map(_ >> attr("href")("a"))
+      val threadLinks: List[String] = threadRelLinks.map { l => baseUrl + l }
 
-    // Extract the thread rows
-    val threadTitlesItems: List[Element] = subForumPage >> elementList(".lia-list-row-thread-unread")
-    val threadTitles: List[String] = threadTitlesItems.map(_ >> text("h3"))
-    val threadRelLinks: List[String] = threadTitlesItems.map(_ >> attr("href")("a"))
-    val threadLinks: List[String] = threadRelLinks.map { l => baseUrl + l}
+      val threadDateTimeItems: List[Element] = subForumPage >> elementList(".DateTime")
+      var threadDates: List[String] = threadDateTimeItems.map(_ >> text(".local-date"))
+      val threadTimes: List[String] = threadDateTimeItems.map(_ >> text(".local-time"))
 
-    val threadDateTimeItems: List[Element] = subForumPage >> elementList(".DateTime")
-    var threadDates: List[String] = threadDateTimeItems.map(_ >> text(".local-date"))
-    val threadTimes: List[String] = threadDateTimeItems.map(_ >> text(".local-time"))
+      // Remove &lrm; character in HTML
+      threadDates = threadDates.map(d => d.replace("\u200E", ""))
+      val threadDateTimes = (threadDates zip threadTimes) map { case (d, t) => convertDate(d) + " " + convertTime(t) }
 
-    // Remove &lrm; character in HTML
-    threadDates = threadDates.map(d => d.replace("\u200E", ""))
-    val threadDateTimes = (threadDates zip threadTimes) map { case (d, t) => convertDate(d) + " " + convertTime(t)}
+      // Extract messages of a thread
+      var threadMessages = new ListBuffer[List[Map[String, String]]]()
+      for (thread <- threadLinks) {
+        val threadPage = browser.get(thread)
+        val threadMessagesItems: List[Element] = threadPage >> elementList(".lia-message-body-content")
 
-    // Extract messages of a thread
-    var threadMessages = new ListBuffer[List[Map[String, String]]]()
-    for (thread <- threadLinks) {
-      val threadPage = browser.get(thread)
-      val threadMessagesItems: List[Element] = threadPage >> elementList(".lia-message-body-content")
-
-      var messages = new ListBuffer[Map[String, String]]()
-      for (threadMessage <- threadMessagesItems) {
-        val message = threadMessage >> extractor(".lia-message-body-content", text)
-        messages += Map(message -> "SENTIMENT")
+        var messages = new ListBuffer[Map[String, String]]()
+        for (threadMessage <- threadMessagesItems) {
+          val message = threadMessage >> extractor(".lia-message-body-content", text)
+          messages += Map(message -> "SENTIMENT")
+        }
+        threadMessages += messages.toList
       }
-      threadMessages += messages.toList
-    }
 
-    // Combine title, link, timestamp and messages of each thread
-    val threadMessagesList = threadMessages.toList
-    val threads = ((threadTitles zip threadLinks) zip threadDateTimes) zip threadMessagesList map {
-      case (((threadTitles, threadLinks), threadDateTimes), threadMessagesList) =>
-        (threadTitles, threadLinks, threadDateTimes, threadMessagesList)
-    }
-    println(threads)
+      // Combine title, link, timestamp and messages of each thread
+      val threadMessagesList = threadMessages.toList
+      val threads = ((threadTitles zip threadLinks) zip threadDateTimes) zip threadMessagesList map {
+        case (((threadTitles, threadLinks), threadDateTimes), threadMessagesList) =>
+          (threadTitles, threadLinks, threadDateTimes, threadMessagesList)
+      }
+      //println(threads)
 
+      val threadsRDD = sc.makeRDD(threads.toSeq)
+      println(threadsRDD)
+      Thread.sleep(60000)
+    }
   }
 
   def convertDate(date: String): String = {
